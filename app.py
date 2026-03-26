ad72359f7994c22902a3218a9dcd9c38ea2707c1
Jemii <jemina.derly@gmail.com>
2026-03-26 19:48:31 +0100

    Remove Supabase connection setup
    Remove Supabase connection code from app.py

--- app.py
+++ app.py
@@ -1,7 +1,1 @@
-import streamlit as st
-from supabase import FOODROP_juju
 
-# Connexion à Supabase
-url = st.secrets["SUPABASE_URL"]
-key = st.secrets["SUPABASE_KEY"]
-supabase = FOODROP_juju(url, key)
